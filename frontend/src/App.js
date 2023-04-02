abf62ec0fcfa3bc483dad238ebc9f188befbbed7
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-02 21:35:18 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,27 @@ function App() {
   return (
     <div className="App">
       <h1>GarudaEye</h1>
+      <p>Eye Of The Wild</p>
+      <div className="About">
+          <h2>About</h2>
+          <p>Lorem ipsum dolor sit amet. Ut libero voluptates non libero galisum aut deserunt enim eos voluptas autem qui quaerat omnis. Aut praesentium doloremque et nemo dolorem aut incidunt nostrum hic odit repellendus. Et voluptas voluptas sed eligendi harum qui tempora amet ex architecto consequatur. Ut atque aperiam in inventore inventore eos illum rerum.
+
+            A consequatur voluptas eum maxime dicta id totam repudiandae est laboriosam aliquid non repudiandae eligendi? Vel harum earum quo aliquid dolores et eius dicta sit vero itaque sed unde sapiente cum maiores quaerat nam reprehenderit nostrum. Rem nihil pariatur et modi internos vel adipisci tempore qui iusto unde et explicabo dolorum eum voluptas totam.
+
+            Et animi impedit a error officia non recusandae accusantium qui quam nobis! Ut commodi velit aut nobis quia quo quis dolore est assumenda sunt. Aut natus sapiente aut autem quam eum Quis natus non quod modi ea ratione molestiae est repellat laudantium. A harum internos et quasi dolor aut quam accusamus qui mollitia recusandae qui voluptatem perferendis.</p>
+      </div>
+      <div className="MobApp">
+      <h1>Mobile App</h1>
+      <p>Lorem ipsum dolor sit amet. Ut libero voluptates non libero galisum aut deserunt enim eos voluptas autem qui quaerat omnis. Aut praesentium doloremque et nemo dolorem aut incidunt nostrum hic odit repellendus. Et voluptas voluptas sed eligendi harum qui tempora amet ex architecto consequatur. Ut atque aperiam in inventore inventore eos illum rerum.
+
+      A consequatur voluptas eum maxime dicta id totam repudiandae est laboriosam aliquid non repudiandae eligendi? Vel harum earum quo aliquid dolores et eius dicta sit vero itaque sed unde sapiente cum maiores quaerat nam reprehenderit nostrum. Rem nihil pariatur et modi internos vel adipisci tempore qui iusto unde et explicabo dolorum eum voluptas totam.</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/IPhone_14_Pro_vector.svg/220px-IPhone_14_Pro_vector.svg.png" alt="phone"></img>
+      </div>
+      <div className="API">
+      <h1>API</h1>
+      Lorem ipsum dolor sit amet. Ut libero voluptates non libero galisum aut deserunt enim eos voluptas autem qui quaerat omnis. Aut praesentium doloremque et nemo dolorem aut incidunt nostrum hic odit repellendus. Et voluptas voluptas sed eligendi harum qui tempora amet ex architecto consequatur. Ut atque aperiam in inventore inventore eos illum rerum.
+      <br/>https://api.spotify.com/v1/albums/id
+      </div>
     </div>
   );
 }
