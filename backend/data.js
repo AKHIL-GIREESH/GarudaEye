7dd12c94da30ec3af0ccd35a3fbaba5af1cadfbe
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-03 01:05:39 +0530

    backend

---
+++ backend/data.js
@@ -0,0 +1,18 @@
+const data = {
+    Kerala1: {
+        elephant:10,
+        tiger:12,
+        animal3:4,
+        animal4:5,
+        animal5:6
+    },
+    Kerala2: {
+        elephant:11,
+        tiger:10,
+        animal3:8,
+        animal4:9,
+        animal5:7
+    }
+}
+
+module.exports = data
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
+const data = require("./data")
 const Express = require("express")
 const express = Express()
+
+express.get("/",(req,res) => {
+    res.status(200).send("<h1>HomePage</h1>")
+})
+express.get("/wildAnimalsData",(req,res) => {
+    res.status(200).json(data)
+})
 
 express.listen(3000,() => console.log("Connected"))
